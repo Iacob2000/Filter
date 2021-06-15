928298a006cc8ae667d797af98d76720d1d75825
Iacob Stelică <stelicaiacob80@yahoo.com>
2021-06-16 00:52:14 +0100

    work

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ProfileScreen from './screens/ProfileScreen';
+import{Provider} from 'react-redux';
+import {createStore,applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './redux/reducers'
+const store = createStore(rootReducer,applyMiddleware(thunk))
+
 import * as firebase from 'firebase';
 const firebaseConfig = {
   apiKey: "AIzaSyBrDXBWu2QL-vKikTJ2Wa0_zuOb98-0Ot8",
@@ -12,6 +19,7 @@ const firebaseConfig = {
   appId: "1:894848608942:web:57149a6147b47e019d5b3f",
   measurementId: "G-W21YM91B45"
 };
+
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
 
@@ -19,10 +27,50 @@ var db = firebase.firestore();
 import LoginScreen from './screens/LoginScreen';
 import FilterNavigation from './navigation/Navigation'
 
-export default function App() {
-  return (
-  <FilterNavigation />
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      loaded: false,
+    }
+  }
+componentDidMount(){
+  firebase.auth().onAuthStateChanged((user) => {
+    if(!user){
+      this.setState({
+        loggedIn:false,
+        loaded:true
+      })
+    }else{
+      this.setState({
+        loggedIn:true,
+        loaded:true
+    })
+  }
+  })
+}
+  render() {
+    const {loggedIn,loaded}= this.state;
+    if(!loaded){
+    return(
+      <View style={{flex:1,justifyContent:'center'}}>
+        <Text>loading</Text>
+      </View>
+    )
+    }
+    if(!loggedIn){
+      
+      return(
+        <FilterNavigation />
+        )
+      }
+      return(
+        <Provider store={store}>
+           <ProfileScreen/>
+        </Provider>
+      )
+      }
+    
 }
 
 const styles = StyleSheet.create({
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {View,Button} from 'react-native';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchUser} from '../redux/actions/index';
+export class ProfileScreen extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
 
-function ProfileScreen(props) {
-    return (
-        <View>
+    }
+    render(){
+    const {currentUser} = this.props;
+    console.log(currentUser  )
+        return (
+    <View>
+ 
+    <Button title= 'Home' onPress = {() => {
+        props.navigation.navigate({routeName : 'Home'});
+     }}/>
+   
      
-     <Button title= 'Home' onPress = {() => {
-            props.navigation.navigate({routeName : 'Home'});
-        }}/>
-       
-         
-      
-    </View>
-        
+  
+     </View>
     )
+    }
+ 
+        
 }
-
-export default ProfileScreen
+const mapStateToProps = (store) => ({
+    currentUser: store.userState.currentUser
+})
+const mapDispatchProps= (dispatch) => bindActionCreators({fetchUser},dispatch)
+  
+export default connect(mapStateToProps, mapDispatchProps)(ProfileScreen);
